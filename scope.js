8c26e627310dd5cede3911cdeea4cb2b29e8f8c0
VIKAS N S <vikasns@VIKASs-MacBook-Air.local>
2020-04-07 12:24:37 +0530

    changes

--- scope.js
+++ scope.js
@@ -1,10 +1,10 @@
 let globalVar = "global";
 
 function local() {
-  let localVar = "local";
+  let localVar = "local"; // if you remove let variable localVar will be globally scoped.
   console.log(globalVar);
   console.log(localVar);
 }
 
 local();
-console.log(localVar);
+// console.log(localVar); // throws error as localVar declared locally.
